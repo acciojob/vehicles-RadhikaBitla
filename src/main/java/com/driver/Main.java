16c35fd24891a4a5b7045ba92718dc5f31b15e96
Radhika Bitla <bitlaradhika3@gmail.com>
2023-04-15 15:12:59 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,9 +3,11 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         F1 mercedes = new F1("mercedes", false);
-        mercedes.move(40, 0);
-        mercedes.accelerate(20);
-        mercedes.accelerate(-60);
-        mercedes.accelerate(90);
+        mercedes.move(100, 30);
+        mercedes.steer(60);
+        mercedes.move(20,30);
+        mercedes.steer(45);
+        mercedes.move(10,45);
+        mercedes.steer(45);
     }
 }
