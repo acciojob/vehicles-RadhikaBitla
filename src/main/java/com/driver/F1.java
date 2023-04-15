05a3b8b465ba36695168f5f503e67f4ce277625a
Radhika Bitla <bitlaradhika3@gmail.com>
2023-04-15 13:05:58 +0530

    till subtask 2

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -2,12 +2,18 @@ package com.driver;
 
 public class F1 extends Car {
 
-    public F1(String name, boolean isManual) {
+    public F1(String name, boolean isManual)
+    {
         //Use arbitrary values for parameters which are not mentioned
+        super(name,0,0, 0, isManual,"",0);
     }
 
+
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        int newSpeed = 0;
+        currentSpeed+=rate;
+        newSpeed=currentSpeed;
+        //set the value of new speed by using currentSpeed and rate
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -17,14 +23,45 @@ public class F1 extends Car {
          * speed 201-250: gear 5
          * speed more than 250: gear 6
          */
-
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
+            stop();
+            changeGear(1);
+        }
+        else if(newSpeed>=1 && newSpeed<=50)
+        {
+            stop();
+            changeGear(1);
+        }
+        else if(newSpeed>=51 && newSpeed<=100)
+        {
+            stop();
+            changeGear(1);
+        }
+        else if(newSpeed>=101 && newSpeed<=150)
+        {
+            stop();
+            changeGear(1);
+        }
+        else if(newSpeed>=151 && newSpeed<=200)
+        {
+            stop();
+            changeGear(1);
+        }
+        else if(newSpeed>=201 && newSpeed<=250)
+        {
+            stop();
+            changeGear(1);
+        }
+        else if(newSpeed>250)
+        {
+            stop();
+            changeGear(6);
         }
         //for all other cases, change the gear accordingly
 
-        if(newSpeed > 0) {
+        /*if(newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
-        }
+        }*/
     }
 }
